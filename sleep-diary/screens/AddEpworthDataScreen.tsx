49c31ab57e3e14e59ab5576bbd64d269c78749df
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2021-04-07 03:36:47 +0100

    Added calendar entry and title to Epworth screen

--- sleep-diary/screens/AddEpworthDataScreen.tsx
+++ sleep-diary/screens/AddEpworthDataScreen.tsx
@@ -1,12 +1,81 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 import { View } from '../components/Themed';
+import moment from "moment";
 
 const AddEpworthData = () => {
+  // Section for showing 'Date for' calendar entry
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
   return (
     <View style={styles.container}>
+      <View style = {styles.screenContent}>
+        <Text style={styles.title}>Enter Epworth data</Text>
+        {
+          /* Same as the one used in Add Sleep Data
+             Used here as well since it's needed for similar goal
+          */
+        }
+        <View style = {styles.dateEntry}>
+          <Text>Data for the night of</Text>
+          <Button
+            style = {styles.button}
+            mode = "contained"
+            labelStyle = {{ color: "black", fontSize: 12}}
+            onPress = {showDatepicker}
+          >
+          {moment(date).format("DD/MM/YY")}
+          </Button>
+
+          <Ionicons
+            size = {30}
+            name = "calendar"
+            style = {{ marginTop: 5, marginLeft: 5 }}
+            onPress = {showDatepicker}
+          />
+
+          {show && (
+            <DateTimePicker
+              value={date}
+              mode={mode}
+              is24Hour={true}
+              display="default"
+              onChange={onChange}
+            />
+          )}
+        </View>
+        <View style={styles.separator} lightColor="black" darkColor="rgba(255,255,255,0.1)" />
+
+        <View style = {styles.questionnaireSection}>
+          <Text>Data for the night of</Text>
+        </View>
+
+      </View>
     </View>
   )
 }
@@ -17,6 +86,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: "#F7E3D9", // Background color for whole screen
+  },
+  title: {
+    fontSize: 35,
+    alignSelf: "center",
+    fontWeight: 'bold',
+  },
+  screenContent: {
+    marginTop: "25%",
+    width: "85%",
+    backgroundColor: "#F7E3D9",
+  },
+  dateEntry: {
+    marginTop: "10%",
+    backgroundColor: "#F7E3D9",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    marginTop: 15,
+    marginBottom: 10,
+    width: "35%",
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+    alignSelf: "center",
+    marginLeft: 10,
+  },
+  separator: {
+    marginVertical: "3%",
+    height: 2,
+    width: '100%',
   },
 });
 
